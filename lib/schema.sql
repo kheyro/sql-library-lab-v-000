f8b756fbc6f489801e4bf058a2da7c590d36c1e3
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 10:34:38 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,10 +2,10 @@ CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
   author_id INTEGER,
-  sub-genre_id INTEGER
+  subgenre_id INTEGER
 );
 
-CREATE TABLE sub-genres (
+CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
   name TEXT
 );
