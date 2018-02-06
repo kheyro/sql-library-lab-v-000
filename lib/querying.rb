4e8756e54e46dc0b30f173227d96524601766897
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 11:16:07 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(*) FROM character_books
-    JOIN characters ON character_books.character_id = characters.id
+  "SELECT characters.name, COUNT(*) FROM characters
+    JOIN character_books ON character_books.character_id = characters.id
     GROUP BY character_books.character_id ORDER BY COUNT(*) DESC"
 end
