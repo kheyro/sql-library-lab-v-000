0066f17c46d851c2bcb9337451edee952343ef32
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 10:47:41 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,12 +2,12 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES ("History series", 1, 
 
 INSERT INTO books (title, year, series_id) VALUES ("Second world war", 1935, 1), ("King Arthur", 1735, 1), ("Castle on the hill", 1830, 1), ("Matrix", 2000, 2), ("Planet", 2001, 2), ("Mars", 2002, 2);
 
-INSERT INTO characters (name, motto, species, author_id, series_id) 
+INSERT INTO characters (name, motto, species, author_id, series_id)
   VALUES ("Superman", "Save the worl", "Alien", 1, 2),
   ("Batman", "In the dark", "Human", 1, 2),
   ("Cat woman", "Miaw miaw", "Human Cat", 1, 2),
   ("Thor", "Blinded by the light", "Human", 2, 2),
-  ("Spider man"), "Webmaster", "Spider", 2, 2), 
+  ("Spider man"), "Webmaster", "Spider", 2, 2),
   ("Snoopy", "Zzzz", "Dog", 2, 2),
   ("Wolverin", "Ahouuuuu", "Wolf", 2, 2),
   ("Electra", "Aie", "Alien", 2, 2)
