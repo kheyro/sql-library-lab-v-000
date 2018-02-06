5441dab82c6f4902e9952b6caff63bff377500a1
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 11:11:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,11 +14,11 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors
     LEFT JOIN series ON series.author_id = authors.id
-    LEFT JOIN subgenres ON series.subgenre_id = subgenres.id"
+    LEFT JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series JOIN characters ON series.id = characters.serie_id WHERE characters.species = 'human' GROUPY BY series.title ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
